d28b64036d056ebdcce63a8dde3be7787c12126d
Daniel Lazzaroni <dlazzaroni@my.bcit.ca>
2022-11-08 15:15:30 -0800

    do/while

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,13 @@ while (i <= 10){
  console.log(2**i)
  i = i+1
 }
+
+let input = ""
+do{
+    input = prompt("What is your name?");
+    console.log(input);
+}
+while(!isNaN(Number(input)))
+
+alert("Hello " + input);
+alert(`Hello ${input}`)
